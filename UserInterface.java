42f1e30c123a1ff1a99dff25eab73240cfeaa2cb
Deng-Hanjun <hd2432@nyu.edu>
2025-12-12 13:37:55 -0500

    implement the search feature

--- UserInterface.java
+++ UserInterface.java
@@ -10,29 +10,34 @@ public class UserInterface {
     public void run() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-        System.out.println("--------------Main Menu----------------");
-        System.out.println("Press 1 to add a new transaction");
-        System.out.println("Press 2 to browse transactions");
-        System.out.println("Press 4 to view reports");
-        System.out.println("Press 5 to exit");
-        int choice = scanner.nextInt();
-        scanner.nextLine(); 
-        if (choice == 1) {
-            AddNewCLI addNewCLI = new AddNewCLI(manager);
-            addNewCLI.enterDetails();
-        } else if (choice == 2) {
-            BrowseCLI browseCLI = new BrowseCLI(manager);
-            browseCLI.browseTransactions();
-        } else if (choice == 4) {
-                ViewReportCLI viewReportCLI = new ViewReportCLI(manager);
-                viewReportCLI.chooseViewReport();   
-        }
-        else if (choice == 5) {
-            System.out.println("Exiting...");
-            break;
-        } else {
-            System.out.println("Invalid choice");
-        }
+            System.out.println("--------------Main Menu----------------");
+            System.out.println("Press 1 to add a new transaction");
+            System.out.println("Press 2 to browse transactions");
+            System.out.println("Press 3 to search transactions");
+            System.out.println("Press 4 to view reports");
+            System.out.println("Press 5 to exit");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); 
+            if (choice == 1) {
+                AddNewCLI addNewCLI = new AddNewCLI(manager);
+                addNewCLI.enterDetails();
+            } else if (choice == 2) {
+                BrowseCLI browseCLI = new BrowseCLI(manager);
+                browseCLI.browseTransactions();
+            } else if (choice == 3) {
+                SearchCLI searchCLI = new SearchCLI(manager);
+                searchCLI.searchMenu();
+            }
+            else if (choice == 4) {
+                    ViewReportCLI viewReportCLI = new ViewReportCLI(manager);
+                    viewReportCLI.chooseViewReport();   
+            }
+            else if (choice == 5) {
+                System.out.println("Exiting...");
+                break;
+            } else {
+                System.out.println("Invalid choice");
+            }
         }
         manager.saveToFile();
         scanner.close();
