c9f0dda60233735f8a4cb8656ede5b76d4f87251
Tonyzsp <sz3822@nyu.edu>
2025-12-12 02:40:27 -0500

    finish record

--- UserInterface.java
+++ UserInterface.java
@@ -13,7 +13,8 @@ public class UserInterface {
         System.out.println("--------------Main Menu----------------");
         System.out.println("Press 1 to add a new transaction");
         System.out.println("Press 2 to browse transactions");
-        System.out.println("Press 3 to exit");
+        System.out.println("Press 4 to view reports");
+        System.out.println("Press 5 to exit");
         int choice = scanner.nextInt();
         scanner.nextLine(); 
         if (choice == 1) {
@@ -22,12 +23,16 @@ public class UserInterface {
         } else if (choice == 2) {
             BrowseCLI browseCLI = new BrowseCLI(manager);
             browseCLI.browseTransactions();
-        } else if (choice == 3) {
+        } else if (choice == 4) {
+                ViewReportCLI viewReportCLI = new ViewReportCLI(manager);
+                viewReportCLI.chooseViewReport();   
+        }
+        else if (choice == 5) {
             System.out.println("Exiting...");
             break;
         } else {
             System.out.println("Invalid choice");
-            }
+        }
         }
         manager.saveToFile();
         scanner.close();
